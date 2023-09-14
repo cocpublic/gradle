472340e935f7761f979a4ff1c5928360f138d8b5
Tom Tresansky <ttresansky@gradle.com>
2023-09-14 09:56:53 -0400

    Correct javadoc - this helps with JavaPlugin

--- platforms/jvm/plugins-java-base/src/main/java/org/gradle/api/plugins/internal/JavaPluginHelper.java
+++ platforms/jvm/plugins-java-base/src/main/java/org/gradle/api/plugins/internal/JavaPluginHelper.java
@@ -20,18 +20,18 @@ import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.component.SoftwareComponent;
 import org.gradle.api.internal.tasks.JvmConstants;
-import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.jvm.JvmTestSuite;
 import org.gradle.jvm.component.internal.JvmSoftwareComponentInternal;
 import org.gradle.testing.base.TestSuite;
 import org.gradle.testing.base.TestingExtension;
 
 /**
- * Utility class intended for use only when the {@link JavaBasePlugin} is applied.
+ * Utility class intended for use only when the {@link org.gradle.api.plugins.JavaPlugin JavaPlugin} is applied.
  *
- * This class exists to avoid adding these methods to the {@code JavaPlugin} itself,
+ * This class exists to avoid adding these methods to the {@code org.gradle.api.plugins.JavaPlugin JavaPlugin} itself,
  * and thus avoids adding these methods to the public API.
  */
+@SuppressWarnings("JavadocReference")
 public class JavaPluginHelper {
     public static final String DEFAULT_TEST_SUITE_NAME = "test";
 
